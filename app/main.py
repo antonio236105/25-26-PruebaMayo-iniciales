95f7e1d8c7d3eddaa281e1dacc7607a7362e093d
Antonio Palacios Hernández <palacioshernandezantonio0489@gmail.com>
2026-05-07 17:03:20 +0200

    he añadido la carpeta app

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from .database import Base, engine
+from .routers import items
+
+Base.metadata.create_all(bind=engine)
+app = FastAPI()
+app.include_router(items.router)
+
+
+# Código que se añade
+@app.get("/status")
+def version():
+    return {"status": "Palacios, Antonio - v.01"}
